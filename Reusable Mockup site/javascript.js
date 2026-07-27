9c1ae9421133a69855cd710ec94dab3b80e18724
Cade Riley <caderiley010@gmail.com>
2026-07-26 20:20:22 -0500

    Add developer comments for template reuse, remove duplicate container rule

--- Reusable Mockup site/javascript.js
+++ Reusable Mockup site/javascript.js
@@ -1,3 +1,7 @@
+// Grabs the hamburger button and the <ul> inside #mainNav.
+// Clicking the button toggles the .open class, which style.css
+// uses to show/hide the mobile nav menu (see @media block)
+
 const navToggle = document.getElementById('navToggle');
 const navMenu = document.querySelector('#mainNav ul');
 
