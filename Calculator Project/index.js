d6a507617f0d847e26e182b59023009bd13ee49c
Cade Ross <caderiley010@gmail.com>
2026-08-07 23:03:54 -0500

    Reformat Calculator files (prettier)

--- Calculator Project/index.js
+++ Calculator Project/index.js
@@ -4,10 +4,10 @@ const tipInput = document.getElementById("tip");
 const totalSpan = document.getElementById("total");
 
 function calculateTotal() {
-    const billValue = billInput.value;
-    const tipValue = tipInput.value;
-    const totalValue = billValue * (1 + tipValue / 100);
-    totalSpan.innerText = totalValue.toFixed(2);
+  const billValue = billInput.value;
+  const tipValue = tipInput.value;
+  const totalValue = billValue * (1 + tipValue / 100);
+  totalSpan.innerText = totalValue.toFixed(2);
 }
 
 btnEl.addEventListener("click", calculateTotal);
